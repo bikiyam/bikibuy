d888ee9e4b6f8db597852bb607ad377ea4962f96
bikiyam <kihlothager@gmail.com>
2025-02-11 03:54:39 -0800

    Initial Commit

--- components/navbar.vue
+++ components/navbar.vue
@@ -25,7 +25,7 @@ import { IconSearch } from '@tabler/icons-vue';
         <div class=" flex place-items-center gap-5">
             <div class=" flex gap-4">
                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"  /></svg>
-                <Carticon :cartcount="4" />
+                <Carticon :cartcount="99" />
             </div>
             <div class="accoutline flex gap-2 place-items-center">
                 <img src="../assets/person.jfif" class=" w-10 rounded-full place-items-center">
--- pages/index.vue
+++ pages/index.vue
@@ -6,75 +6,8 @@ import Category from '~/components/category.vue';
 
 <template>
     <div class="flex flex-col gap-5">
-        <div class="showoff w-screen h-[calc(100vh-150px)] flex gap-5 px-8 overflow-x-scroll overflow-y-hidden ">
-            <div class="featured flex flex-col w-[500px] min-w-[500px] p-5 h-full bg-gray-500 rounded-3xl relative">
-                <img src="../assets/images/bodybuilder.png" alt="" class="h-full rounded-3xl absolute top-0 left-0">
-                <div class="px-3 py-1 bg-white text-black z-10 rounded-lg w-fit text-sm font-semibold">
-                    exclusive
-                </div>
-                <div class="specs z-10 mt-auto flex flex-col gap-2">
-                    <h1 class="text-white text-[40px] font-extrabold leading-[35px]">Japanese Indoor Chair</h1>
-                    <h2 class="text-white text-[15px]">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    </h2>
-                    <div class="flex mt-[1px] place-items-center gap-2">
-                        <div class="px-4 py-2 bg-white text-black z-10 rounded-3xl w-fit text-[13px] font-semibold">
-                            <h1>View Product</h1>
-                        </div>
-                        <div class="arrowtopright text-black bg-white w-9 h-9 rounded-full flex place-items-center justify-center ml-[-2px]">
-                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 7l-10 10" /><path d="M8 7l9 0l0 9" /></svg>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="slogan flex flex-col w-[500px] p-5 py-6  h-full rounded-xl min-w-[500px]">
-                <h1 class=" text-[42px] font-[700] leading-[50px] text-[#494444]">
-                    Discover the Essence minimalist furniture design & style 
-                </h1>
-                <div class="explore mt-auto w-full h-[240px] flex flex-col gap-2">
-                    <div class="list ml-2 flex gap-5 text-[#494444] text-[13px]">
-                        <div class="ex text-[black] font-semibold">
-                            exclusive
-                        </div>
-                        <div class="de">
-                            Deluxe
-                        </div>
-                        <div class="in">
-                            Indoor
-                        </div>
-                        <div class="fur">
-                            Furniture
-                        </div>
-                    </div>
-                    <div class="card p-5 bg-white w-full h-full rounded-xl flex">
-                        <div class="leftpart h-full w-1/2 flex flex-col place-content-between">
-                            <div class="px-4 py-2  bg-gray-200 text-black z-10 rounded-3xl w-fit text-[13px] font-semibold">
-                                Exsclusive
-                            </div>
-                            <div>
-                                <h1 class=" font-bold text-[19px]">
-                                    Pure Space Focus Duo
-                                </h1>
-                                <h3 class="text-[13px]">
-                                    Sleek Minimalist design for Ultimate Productivity and  Comfort
-                                </h3>
-                            </div>
-                            <div class="arrowtopright text-black bg-gray-200 w-9 h-9 rounded-full flex place-items-center justify-center ml-[-2px]">
-                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 7l-10 10" /><path d="M8 7l9 0l0 9" /></svg>
-                            </div>
-                        </div>
-                            <img src="../assets/images/nike.png" alt="" class="w-[180px] h-full ml-auto rounded-md">
-                    </div>
-                </div>
-            </div>
-            <img src="../assets/images/run.png" alt="" class="w-[320px] h-full rounded-xl">
-            <img src="../assets/images/range.png" alt="" class="w-[320px] h-full rounded-xl">
-            <img src="../assets/images/circle.jfif" alt="" class="w-[320px] h-full rounded-xl">
-            <img src="../assets/images/supra.png" alt="" class="w-[320px] h-full rounded-xl">
-        </div>
-
         <div>
+            <Showoff/>
             <Category/>
         </div> 
 
